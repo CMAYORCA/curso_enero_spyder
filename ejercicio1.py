09ea351e8aeeaa7beff95b88784c292c07992ab4
CMAYORCA <cmayorca1407@gmail.com>
2025-01-16 21:32:53 -0500

    Realizar ejercicio 2

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,24 @@
 import pandas as pd
 import numpy as np
 
-datos= {'Nombre': ['Leonardo', 'Joaquin', 'Alex', 'Paula', 'Enrique', 'Jhon'],
-        'Materias': ['Lenguaje', 'EDF', 'Quimica', 'Matematicas', 'Ingles','Historia'],
-        'Calificaciones': ['18','20', '13','19' ,'16', '17'],
-        'Deportes':['Futbol','Natacion', 'Tenis', 'Voley', 'Natacion', 'Balon mano']
+datos= {'Nombre': ['Leonardo', 'Joaquin', 'N/A', 'Paula', 'Enrique', 'Jhon'],
+        'Materias': ['Lenguaje', 'EDF', 'Quimica', 'Matematicas', 'Ingles','N/A'],
+        'Calificaciones': ['18','20', np.nan,'19' ,'16', '17'],
+        'Deportes':['Futbol','Natacion', 'Tenis', 'Voley', 'N/A', 'Balon mano']
         }
 df= pd.DataFrame(datos)
 print(df)
+print('n*4')
+
+#DATOS NO VALIDOS
+
+datos2 = {'Nombre': ['Leonardo', 'Joaquin', 'N/A', 'Paula', 'Enrique', 'Jhon'],
+        'Materias': ['Lenguaje', 'EDF', 'Quimica', 'Matematicas', 'Ingles','N/A'],
+        'Calificaciones': ['18','20', np.nan,'19' ,'16', '17'],
+        'Deportes':['Futbol','Natacion', 'Tenis', 'Voley', 'N/A', 'Balon mano']
+        }
+
+df2 = pd.Dataframe(datos2)
+print(df2)
+print('/n'*1)
+print(df2.info()) 
