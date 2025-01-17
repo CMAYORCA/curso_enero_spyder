781d9b069783cb4a019bfe88d7b58e0bf46e6c28
CMAYORCA <cmayorca1407@gmail.com>
2025-01-16 21:01:51 -0500

    Realizar ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,10 @@
-print("HOla")
+import pandas as pd
+import numpy as np
+
+datos= {'Nombre': ['Leonardo', 'Joaquin', 'Alex', 'Paula', 'Enrique', 'Jhon'],
+        'Materias': ['Lenguaje', 'EDF', 'Quimica', 'Matematicas', 'Ingles','Historia'],
+        'Calificaciones': ['18','20', '13','19' ,'16', '17'],
+        'Deportes':['Futbol','Natacion', 'Tenis', 'Voley', 'Natacion', 'Balon mano']
+        }
+df= pd.DataFrame(datos)
+print(df)
